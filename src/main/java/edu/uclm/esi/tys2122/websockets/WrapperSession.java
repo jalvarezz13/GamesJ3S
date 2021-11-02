2da4348e7d8c7dbc6015427daa366b5d74190cb2
Javier Álvarez <javier.alvarez13@alu.uclm.es>
2021-11-02 19:09:23 +0100

    Actualizado

---
+++ src/main/java/edu/uclm/esi/tys2122/websockets/WrapperSession.java
@@ -0,0 +1,33 @@
+package edu.uclm.esi.tys2122.websockets;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.socket.WebSocketSession;
+
+public class WrapperSession {
+
+	private WebSocketSession wsSession;
+	private HttpSession httpSession;
+
+	public WrapperSession(WebSocketSession wsSession) {
+		this.wsSession = wsSession;
+	}
+
+	public void setHttpSession(HttpSession httpSession) {
+		this.httpSession = httpSession;
+	}
+
+	public WebSocketSession getWsSession() {
+		return wsSession;
+	}
+
+	public void setWsSession(WebSocketSession wsSession) {
+		this.wsSession = wsSession;
+	}
+
+	public HttpSession getHttpSession() {
+		return httpSession;
+	}
+
+	
+}
