7dc243a5221858af4a615253220d5e6ecd91e1f4
Javier Álvarez <javier.alvarez13@alu.uclm.es>
2021-11-02 18:56:33 +0100

    Vuelta a empezar

--- src/main/java/edu/uclm/esi/tys2122/dao/LoginRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.uclm.esi.tys2122.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.uclm.esi.tys2122.model.Login;
-
-public interface LoginRepository extends JpaRepository <Login, String> {
-
-}
