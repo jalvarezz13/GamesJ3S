0db6936d7d9e1176c95860a9c3dd507edba4991f
Juan Muñoz Calvo <Juan.Munoz7@alu.uclm.es>
2022-01-14 12:49:31 +0100

    Enviar mensajes con intro, comprobar mensjaes vacíos y scroll bottom al enviarlos

--- src/main/webapp/js/viewModels/games.js
+++ src/main/webapp/js/viewModels/games.js
@@ -1,11 +1,11 @@
-define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils', 'jquery', 'utils/routes'], function (
-  ko,
-  app,
-  moduleUtils,
-  accUtils,
-  $,
-  routesFile,
-) {
+define([
+  "knockout",
+  "appController",
+  "ojs/ojmodule-element-utils",
+  "accUtils",
+  "jquery",
+  "utils/routes",
+], function (ko, app, moduleUtils, accUtils, $, routesFile) {
   class MenuViewModel {
     constructor() {
       let self = this;
@@ -26,7 +26,10 @@ define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils', '
 
       //Chat
       self.chat = ko.observableArray([
-        { user: 'GamesJ3S-Bot🤖', msg: 'Bienvenido a GamesJ3S, escribe en el chat para comunicarte con el resto de jugadores.😉' },
+        {
+          user: "GamesJ3S-Bot🤖",
+          msg: "Bienvenido a GamesJ3S, escribe en el chat para comunicarte con el resto de jugadores.😉",
+        },
       ]);
 
       self.inputChat = ko.observable(null);
@@ -38,7 +41,7 @@ define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils', '
       });
       moduleUtils
         .createView({
-          viewPath: 'views/header.html',
+          viewPath: "views/header.html",
         })
         .then(function (view) {
           self.headerConfig({
@@ -53,7 +56,7 @@ define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils', '
       let self = this;
 
       let data = {
-        type: 'get',
+        type: "get",
         url: self.routes.getStatistics,
         success: function (response) {
           self.statistics(response);
@@ -67,7 +70,7 @@ define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils', '
       let self = this;
 
       let data = {
-        type: 'get',
+        type: "get",
         url: self.routes.getGames,
         success: function (response) {
           self.games(response);
@@ -82,11 +85,16 @@ define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils', '
     connectChat() {
       let self = this;
 
-      let ws = new WebSocket(`ws://${window.location.origin.split('//')[1]}/${self.routes.webSocketChat}`);
+      let ws = new WebSocket(
+        `ws://${window.location.origin.split("//")[1]}/${
+          self.routes.webSocketChat
+        }`
+      );
       ws.onopen = function (event) {};
       ws.onmessage = function (event) {
         let msg = JSON.parse(event.data);
         self.addMsgChat(msg);
+        self.handleScrollBottom();
       };
     }
 
@@ -99,18 +107,18 @@ define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils', '
 
     sendMessage() {
       let self = this;
-      if (self.inputChat() != '') {
+      if (self.inputChat() != "" && !/^\s+$/.test(self.inputChat())) {
         let info = {
           msg: self.inputChat(),
         };
 
         let data = {
-          type: 'post',
+          type: "post",
           url: self.routes.sendMessage,
           data: JSON.stringify(info),
-          contentType: 'application/json',
+          contentType: "application/json",
           success: function (response) {
-            self.inputChat('');
+            self.inputChat("");
           },
           error: function (response) {},
         };
@@ -118,9 +126,19 @@ define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils', '
       }
     }
 
+    handleScrollBottom() {
+      var objDiv = document.getElementById("chatContent");
+      objDiv.scrollTop = objDiv.scrollHeight;
+    }
+
+    handleEnterChat(d, e) {
+      let self = this;
+      if (e.keyCode === 13) self.sendMessage();
+    }
+
     connected() {
-      accUtils.announce('Juegos.');
-      document.title = 'Juegos';
+      accUtils.announce("Juegos.");
+      document.title = "Juegos";
 
       let self = this;
 
@@ -134,27 +152,27 @@ define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils', '
       let self = this;
 
       let data = {
-        type: 'get',
-        url: self.routes.joinGame + game.name + '&' + self.tempName(),
+        type: "get",
+        url: self.routes.joinGame + game.name + "&" + self.tempName(),
         success: function (response) {
-          if (game.name == 'Tres en raya') {
+          if (game.name == "Tres en raya") {
             response = {
               ...response,
               x: null,
               y: null,
-              gameError: '',
+              gameError: "",
             };
             self.matches.push(response);
             self.conectarAWebSocket();
           }
 
-          if (game.name == 'Las damas') {
+          if (game.name == "Las damas") {
             response = {
               ...response,
               pieces: [],
               chosenPiece: [],
-              playerColor: 'BLANCO',
-              gameError: '',
+              playerColor: "BLANCO",
+              gameError: "",
             };
             self.matches.push(response);
             self.conectarAWebSocket();
@@ -171,18 +189,23 @@ define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils', '
     // BOTH: Syncronizing the game via WebSocket messages
     conectarAWebSocket() {
       let self = this;
-      let ws = new WebSocket(`ws://${window.location.origin.split('//')[1]}/${self.routes.webSocket}`);
+      let ws = new WebSocket(
+        `ws://${window.location.origin.split("//")[1]}/${self.routes.webSocket}`
+      );
       ws.onopen = function (event) {};
       ws.onmessage = function (event) {
         let msg = JSON.parse(event.data);
-        if (msg.type == 'MATCH UPDATE') {
-          let gameName = self.matches().find((match) => match.id == msg.matchId).game;
-          if (gameName == 'CheckersMatch') {
+        if (msg.type == "MATCH UPDATE") {
+          let gameName = self
+            .matches()
+            .find((match) => match.id == msg.matchId).game;
+          if (gameName == "CheckersMatch") {
             self.updateAlivePieces(msg.matchId);
           }
           self.reload(msg.matchId);
         }
-        if (msg.type == 'MATCH READY' || msg.type == 'MATCH FINISH') self.reload(msg.matchId);
+        if (msg.type == "MATCH READY" || msg.type == "MATCH FINISH")
+          self.reload(msg.matchId);
       };
     }
 
@@ -190,18 +213,21 @@ define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils', '
     mover(match, movement) {
       let self = this;
       let info;
-      if (match.game == 'TictactoeMatch') {
+      if (match.game == "TictactoeMatch") {
         info = {
           x: match.x,
           y: match.y,
           matchId: match.id,
         };
       }
-      if (match.game == 'CheckersMatch') {
-        const [movementX, movementY] = self.getSquareByDirection(match.possibleMovementsXY, movement);
+      if (match.game == "CheckersMatch") {
+        const [movementX, movementY] = self.getSquareByDirection(
+          match.possibleMovementsXY,
+          movement
+        );
         info = {
-          pieceId: match.chosenPiece.toString().split(' ')[0],
-          pieceColor: match.chosenPiece.toString().split(' ')[1],
+          pieceId: match.chosenPiece.toString().split(" ")[0],
+          pieceColor: match.chosenPiece.toString().split(" ")[1],
           movementX: movementX.toString(),
           movementY: movementY.toString(),
           direction: movement,
@@ -210,10 +236,10 @@ define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils', '
       }
 
       let data = {
-        type: 'post',
+        type: "post",
         url: self.routes.move,
         data: JSON.stringify(info),
-        contentType: 'application/json',
+        contentType: "application/json",
         success: function (response) {},
         error: function (response) {
           match.gameError = response.responseJSON.message;
@@ -228,27 +254,27 @@ define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils', '
       let self = this;
 
       let data = {
-        type: 'get',
+        type: "get",
         url: self.routes.findMatch + matchId,
         success: function (response) {
           let actualMatch = self.matches().find((match) => match.id == matchId);
           let newMatch;
-          if (actualMatch.game == 'TictactoeMatch') {
+          if (actualMatch.game == "TictactoeMatch") {
             newMatch = {
               ...response,
               x: null,
               y: null,
-              gameError: '',
+              gameError: "",
             };
           }
 
-          if (actualMatch.game == 'CheckersMatch') {
+          if (actualMatch.game == "CheckersMatch") {
             newMatch = {
               ...response,
               pieces: actualMatch.pieces,
-              chosenPiece: ['Seleccione...'],
+              chosenPiece: ["Seleccione..."],
               playerColor: actualMatch.playerColor,
-              gameError: '',
+              gameError: "",
             };
           }
           self.updateMatch(matchId, newMatch);
@@ -286,11 +312,12 @@ define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils', '
       let self = this;
       let actualMatch = self.matches().find((match) => match.id == matchId);
       if (actualMatch.ready == false) {
-        actualMatch.gameError = 'No puedes eliminar una partida mientras estás esperando';
+        actualMatch.gameError =
+          "No puedes eliminar una partida mientras estás esperando";
         this.updateMatch(matchId, actualMatch);
       } else {
         let data = {
-          type: 'get',
+          type: "get",
           url: self.routes.surrender + matchId,
           success: function (response) {
             self.reload(matchId);
@@ -308,16 +335,17 @@ define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils', '
       let self = this;
 
       let data = {
-        type: 'get',
+        type: "get",
         url: self.routes.updateAlivePieces + matchId,
         success: function (response) {
           let actualMatch = self.matches().find((match) => match.id == matchId);
 
           const [pieces, color] = self.parsePieces(response);
           actualMatch.pieces = pieces;
-          actualMatch.chosenPiece = ['Seleccione...'];
-          actualMatch.playerColor = color == undefined ? actualMatch.playerColor : color;
-          actualMatch.gameError = '';
+          actualMatch.chosenPiece = ["Seleccione..."];
+          actualMatch.playerColor =
+            color == undefined ? actualMatch.playerColor : color;
+          actualMatch.gameError = "";
 
           self.updateMatch(matchId, actualMatch);
         },
@@ -331,23 +359,30 @@ define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils', '
     // CHECKERS: Prints in a different color the possible places to move a piece
     showPossibleMovements(match) {
       let self = this;
-      if (match.chosenPiece != 'Seleccione...' && match.chosenPiece.toString().split(' ')[0] != '') {
+      if (
+        match.chosenPiece != "Seleccione..." &&
+        match.chosenPiece.toString().split(" ")[0] != ""
+      ) {
         let info = {
           matchId: match.id,
-          pieceId: match.chosenPiece.toString().split(' ')[0],
-          pieceColor: match.chosenPiece.toString().split(' ')[1],
+          pieceId: match.chosenPiece.toString().split(" ")[0],
+          pieceColor: match.chosenPiece.toString().split(" ")[1],
         };
         let data = {
-          type: 'post',
+          type: "post",
           url: self.routes.showPossibleMovements,
           data: JSON.stringify(info),
-          contentType: 'application/json',
+          contentType: "application/json",
           success: function (response) {
             match.board = response.board;
             match.possibleMovements = response.possibleMovements;
             match.possibleMovementsXY = response.possibleMovementsXY;
 
-            match.gameError = match.possibleMovementsXY.every((movement) => movement == null) ? 'No se puede mover esta ficha' : '';
+            match.gameError = match.possibleMovementsXY.every(
+              (movement) => movement == null
+            )
+              ? "No se puede mover esta ficha"
+              : "";
 
             self.updateMatch(match.id, match);
           },
@@ -377,13 +412,13 @@ define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils', '
     // CHECKERS: Given a direction returns the corresponding cell
     getSquareByDirection(squares, movement) {
       switch (movement) {
-        case 'leftUp':
+        case "leftUp":
           return squares[0];
-        case 'rightUp':
+        case "rightUp":
           return squares[1];
-        case 'rightDown':
+        case "rightDown":
           return squares[2];
-        case 'leftDown':
+        case "leftDown":
           return squares[3];
       }
     }
